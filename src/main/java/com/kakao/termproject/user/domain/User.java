234038dc6ae197bc61a6a73848830c20cc28f382
happyGogildong <rbgus630@gmail.com>
2025-09-04 20:19:37 +0900

    <feat>(user): User Domain Create

---
+++ src/main/java/com/kakao/termproject/user/domain/User.java
@@ -0,0 +1,29 @@
+package com.kakao.termproject.user.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+}
