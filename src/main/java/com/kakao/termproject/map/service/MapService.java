292382578598dd7cd1f1e95c1308a7a9d4ca8470
ensalada.de.pollo <ensalada.de.pollo@90.local>
2025-09-11 15:17:42 +0900

    feat(map): 경사도 계산 기능 추가, 최고 경사도와 평균 경사도를 반환

---
+++ src/main/java/com/kakao/termproject/map/service/MapService.java
@@ -0,0 +1,122 @@
+package com.kakao.termproject.map.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kakao.termproject.exception.custom.JsonParseException;
+import com.kakao.termproject.map.dto.Coordinate;
+import com.kakao.termproject.map.dto.MapRequest;
+import com.kakao.termproject.map.dto.MapResponse;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+@RequiredArgsConstructor
+public class MapService {
+
+  private final RestTemplate restTemplate;
+  private final ObjectMapper objectMapper;
+
+  public MapResponse getFitness(MapRequest request) {
+    JsonNode response = getResponse(request.parameter());
+    List<Double> elevations = getElevations(response);
+    List<Double> distances = getDistances(response);
+
+    return calcAvgAndMaxSlope(elevations, distances);
+  }
+
+  private JsonNode getResponse(String parameter) {
+    URI uri = UriComponentsBuilder
+      .fromUriString("https://maps.googleapis.com/maps/api/elevation/json")
+      .queryParam("locations", parameter)
+      .build()
+      .toUri();
+
+    String response = restTemplate.getForObject(uri, String.class);
+
+    try {
+      return objectMapper.readTree(response)
+        .get("results");
+    } catch (JsonProcessingException e) {
+      throw new JsonParseException("Google Api 반환 데이터 직렬화에 실패하였습니다.");
+    }
+  }
+
+  private List<Double> getElevations(JsonNode data) {
+    List<Double> elevations = new ArrayList<>();
+
+    for (JsonNode elevation : data) {
+      elevations.add(elevation.get("elevation").asDouble());
+    }
+
+    return elevations;
+  }
+
+  private List<Double> getDistances(JsonNode data) {
+    List<Coordinate> coordinates = new ArrayList<>();
+    List<Double> distances = new ArrayList<>();
+
+    for (JsonNode coordinate : data) {
+      coordinates.add(
+        new Coordinate(
+          coordinate.get("latitude").asDouble(),
+          coordinate.get("longitude").asDouble()
+        )
+      );
+    }
+
+    for (int i = 0; i < coordinates.size() - 1; i++) {
+      distances.add(haversineDistance(coordinates.get(i), coordinates.get(i + 1)));
+    }
+
+    return distances;
+  }
+
+  private MapResponse calcAvgAndMaxSlope(List<Double> elevations, List<Double> distances) {
+    double maxSlope = 0.0;
+    double sumSlope = 0.0;
+
+    for (int i = 0; i < elevations.size() - 1; i++) {
+      double currentSlope = (elevations.get(i + 1) - elevations.get(i)) / distances.get(i);
+      currentSlope = Math.abs(currentSlope);
+
+      sumSlope += currentSlope;
+
+      if (currentSlope > maxSlope) {
+        maxSlope = currentSlope;
+      }
+    }
+
+    double avgOfSlope = (sumSlope / (elevations.size() - 1));
+
+    avgOfSlope *= 100;
+    maxSlope *= 100;
+
+    return new MapResponse(avgOfSlope, maxSlope);
+  }
+
+  private Double haversineDistance(Coordinate location1, Coordinate location2) {
+    final int RADIUS = 6371000; // earth radius meter
+
+    double latDistance = Math.toRadians(location2.latitude() - location1.latitude());
+    double lonDistance = Math.toRadians(location2.longitude() - location1.longitude());
+
+    double sinLatHalfSquared = Math.sin(latDistance / 2) * Math.sin(latDistance / 2);
+    double sinLonHalfSquared = Math.cos(lonDistance / 2) * Math.cos(lonDistance / 2);
+
+    double location1Radians = Math.toRadians(location1.latitude());
+    double location2Radians = Math.toRadians(location2.latitude());
+
+    double a = sinLatHalfSquared + Math.cos(location1Radians)
+      * Math.cos(location2Radians) * sinLonHalfSquared;
+
+    double centralAngle = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return RADIUS * centralAngle; // distance between location1 and location2
+  }
+}
