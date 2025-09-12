c590abb03419cd7beefa0161813e75d933ffaa77
happyGogildong <rbgus630@gmail.com>
2025-09-12 16:07:59 +0900

    feat(Pet): add PetController - CRUD operations handled in PetController (except create) - Pet creation and assignment delegated to UserController

--- src/main/java/com/kakao/termproject/user/service/UserService.java
+++ src/main/java/com/kakao/termproject/user/service/UserService.java
@@ -3,11 +3,15 @@ package com.kakao.termproject.user.service;
 import com.kakao.termproject.exception.custom.EmailDuplicationException;
 import com.kakao.termproject.exception.custom.InvalidPasswordException;
 import com.kakao.termproject.exception.custom.UserNotFoundException;
+import com.kakao.termproject.pet.domain.Pet;
+import com.kakao.termproject.pet.dto.PetCreateRequest;
+import com.kakao.termproject.pet.service.PetService;
 import com.kakao.termproject.user.domain.User;
 import com.kakao.termproject.user.dto.LoginRequest;
 import com.kakao.termproject.user.dto.RegisterRequest;
 import com.kakao.termproject.user.jwt.JwtUtil;
 import com.kakao.termproject.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
   private final UserRepository userRepository;
+  private final PetService petService;
   private final JwtUtil jwtUtil;
 
   public String register(RegisterRequest request) {
@@ -43,5 +48,14 @@ public class UserService {
       throw new InvalidPasswordException("비밀번호가 다릅니다");
     }
     return jwtUtil.createAccessToken(storedUser);
+  }
+
+  @Transactional
+  public void setPet(Long userId, PetCreateRequest request){
+    Pet pet = petService.create(request);
+    User user = userRepository.findById(userId).
+        orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다"));
+
+    user.setPet(pet);
   }
 }
