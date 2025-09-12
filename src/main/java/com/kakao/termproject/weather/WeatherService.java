d795ddc7834a5b9b056de136f34ea2131594895a
Jaehyeok Kim <bbabbasi0505@naver.com>
2025-09-12 15:21:56 +0900

    feat(weather): implement walk score calculation

--- src/main/java/com/kakao/termproject/weather/WeatherService.java
+++ src/main/java/com/kakao/termproject/weather/WeatherService.java
@@ -117,6 +117,30 @@ public class WeatherService {
 
   private int calculateWalkScore(WeatherDetailInternal detail) {
     int score = 0;
+
+    //기온/습도
+    double temperature = detail.temperature();
+    if (temperature > 25) {
+      score -= (int) ((temperature - 25) * 5); // 25℃ 초과 시 1℃당 -5점
+    }
+
+    double humidity = detail.humidity();
+    if (humidity > 60) {
+      score -= (int) (((humidity - 60) / 5) * 3); // 60% 초과 시 5%당 -3점
+    }
+
+    //대기질
+    double pm2_5 = detail.pm2_5();
+    if (pm2_5 >= 76) {
+      score -= 40;
+    } else if (pm2_5 >= 36) {
+      score -= 20;
+    } else if (pm2_5 >= 16) {
+      score += 0;
+    } else if (pm2_5 >= 0) {
+      score += 10;
+    }
+
     return score;
   }
 
