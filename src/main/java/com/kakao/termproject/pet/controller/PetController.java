70dd04966b2ba93f2b75d6ce54514a022478fa04
happyGogildong <rbgus630@gmail.com>
2025-09-15 21:00:35 +0900

    refactor(Pet): fix url /api/pet -> /api/pets

--- src/main/java/com/kakao/termproject/pet/controller/PetController.java
+++ src/main/java/com/kakao/termproject/pet/controller/PetController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/pet")
+@RequestMapping("/api/pets")
 @RequiredArgsConstructor
 @Slf4j
 public class PetController {
