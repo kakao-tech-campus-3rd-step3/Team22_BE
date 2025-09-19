dba6794ddb127edd00fa013fe96507fee899aa29
happyGogildong <rbgus630@gmail.com>
2025-09-19 17:22:17 +0900

    refactor(Security): add authentication path - move password encoding logic UserService -> SecurityConfig

--- src/main/java/com/kakao/termproject/config/SecurityConfig.java
+++ src/main/java/com/kakao/termproject/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.kakao.termproject.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,10 +30,27 @@ public class SecurityConfig {
       .authorizeHttpRequests((auth) -> auth
         .requestMatchers("/ws/**", "/api/dev/**").permitAll()
         .requestMatchers("/", "/index.html", "/error").permitAll()
+        .requestMatchers("/api/user/").authenticated()
         .anyRequest().permitAll())
       .sessionManagement((session) ->
         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+    http.
+        addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
     return http.build();
   }
+
+  @Bean
+  public PasswordEncoder encoder(){
+    return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration
+  ) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
 }
