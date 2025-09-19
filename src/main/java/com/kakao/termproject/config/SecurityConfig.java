911f4893f632b0cbb7e7a62a9fb5ea83bf3ff41a
happyGogildong <rbgus630@gmail.com>
2025-09-19 17:53:20 +0900

    refactor(SecurityConfig): use BCryptPasswordEncoder instead of Argon2
    - BCrypt is faster than Argon2
    - Service does not handle highly sensitive user information

--- src/main/java/com/kakao/termproject/config/SecurityConfig.java
+++ src/main/java/com/kakao/termproject/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,7 +43,7 @@ public class SecurityConfig {
 
   @Bean
   public PasswordEncoder encoder(){
-    return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    return new BCryptPasswordEncoder();
   }
 
   @Bean
