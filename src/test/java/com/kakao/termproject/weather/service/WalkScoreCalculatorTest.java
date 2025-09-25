8ffff342970b42ee7580ab2614fec2b1871192d9
Jaehyeok Kim <bbabbasi0505@naver.com>
2025-09-25 17:56:55 +0900

    test(walkscore): Add low temperature test and refactor test code

--- src/test/java/com/kakao/termproject/weather/service/WalkScoreCalculatorTest.java
+++ src/test/java/com/kakao/termproject/weather/service/WalkScoreCalculatorTest.java
@@ -13,19 +13,31 @@ class WalkScoreCalculatorTest {
 
   private final static int DEFAULT_SCORE = 50;
   private static final LocalDateTime FIXED_TIME = LocalDateTime.of(2025, 9, 19, 0, 0);
+  private static final double HIGH_TEMP_THRESHOLD = 25.0;
+  private static final double LOW_TEMP_THRESHOLD = 4.0;
+  private static final int HIGH_HUMIDITY_THRESHOLD = 60;
+  public static final double PM25_MODERATE = 25.0; // 보통 (16-35) 등급의 대표값
 
-  WeatherDetailInternal baseDetail = new WeatherDetailInternal(
-      FIXED_TIME, 25.0, 60, WeatherCondition.CLEAR_DAY,
-      0, 1.86, 81, 2, 16.0);
+  private WeatherDetailInternal createWeatherDetail(double temp, int humidity, double pm2_5) {
+    return new WeatherDetailInternal(
+        FIXED_TIME,
+        temp,
+        humidity,
+        WeatherCondition.CLEAR_DAY, 0, 1.86, 81, 2,
+        pm2_5
+    );
+  }
 
   @Test
-  public void 테스트1() throws Exception {
+  public void 여러_패널티_조건이_중첩() throws Exception {
     //given
-    // 온도 +6 -> 30점 감점. 습도 -10 -> 변동없음. pm2_5 55 나쁨 -> 20점 감점
-    WeatherDetailInternal detail = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature() + 6.5, baseDetail.humidity() - 10,
-        baseDetail.weather(), baseDetail.precipitationProbability(),
-        baseDetail.windSpeed(), baseDetail.windDegree(), 4, 55);
+    // 온도 +6.5 -> 6*5 = 30점 감점.
+    // 습도 -10 -> 변동없음.
+    // 대기질 pm2_5 55. 나쁨 -> 20점 감점.
+    // 총 50점 감점.
+    WeatherDetailInternal detail = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD + 6.5, HIGH_HUMIDITY_THRESHOLD - 10, 55
+    );
 
     //when
     int walkScore = walkScoreCalculator.calculateWalkScore(detail);
@@ -37,10 +49,9 @@ class WalkScoreCalculatorTest {
   @Test
   public void 기본점수는_50점() throws Exception {
     //given
-    WeatherDetailInternal detail = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(),
-        baseDetail.windSpeed(), baseDetail.windDegree(), baseDetail.aqi(), baseDetail.pm2_5());
+    WeatherDetailInternal detail = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD, PM25_MODERATE
+    );
 
     //when
     int walkScore = walkScoreCalculator.calculateWalkScore(detail);
@@ -53,16 +64,35 @@ class WalkScoreCalculatorTest {
   public void 온도가_25도_초과시_1도당_5점_감점() throws Exception {
     //given
     //기온 +1도 -> 5점 감점
-    WeatherDetailInternal detail1 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature() + 1, baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), baseDetail.aqi(), baseDetail.pm2_5());
+    WeatherDetailInternal detail1 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD + 1, HIGH_HUMIDITY_THRESHOLD, PM25_MODERATE
+    );
 
     //기온 +2.99도 -> 5*2 = 10점 감점
-    WeatherDetailInternal detail2 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature() + 2.99, baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), baseDetail.aqi(), baseDetail.pm2_5());
+    WeatherDetailInternal detail2 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD + 2.99, HIGH_HUMIDITY_THRESHOLD, PM25_MODERATE
+    );
+
+    //when
+    int walkScore1 = walkScoreCalculator.calculateWalkScore(detail1);
+    int walkScore2 = walkScoreCalculator.calculateWalkScore(detail2);
+
+    //then
+    assertThat(walkScore1).isEqualTo(DEFAULT_SCORE - 5);
+    assertThat(walkScore2).isEqualTo(DEFAULT_SCORE - 10);
+  }
+
+  @Test
+  public void 온도가_4도_미만시_1도당_5점_감점() throws Exception {
+    //given
+    //기온 -1도 -> 5점 감점
+    WeatherDetailInternal detail1 = createWeatherDetail(
+        LOW_TEMP_THRESHOLD - 1, HIGH_HUMIDITY_THRESHOLD, PM25_MODERATE
+    );
+    //기온 -2.99도 -> 5*2 = 10점 감점
+    WeatherDetailInternal detail2 = createWeatherDetail(
+        LOW_TEMP_THRESHOLD - 2.99, HIGH_HUMIDITY_THRESHOLD, PM25_MODERATE
+    );
 
     //when
     int walkScore1 = walkScoreCalculator.calculateWalkScore(detail1);
@@ -77,16 +107,13 @@ class WalkScoreCalculatorTest {
   public void 습도_60초과시_5당_3점_감점() throws Exception {
     //given
     //습도 +5 -> 3점 감점
-    WeatherDetailInternal detail1 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity() + 5,
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), baseDetail.aqi(), baseDetail.pm2_5());
-
+    WeatherDetailInternal detail1 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD + 5, PM25_MODERATE
+    );
     //습도 +24 -> 24//5 = 4, 4*3=12 -> 12점 감점
-    WeatherDetailInternal detail2 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity() + 24,
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), baseDetail.aqi(), baseDetail.pm2_5());
+    WeatherDetailInternal detail2 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD + 24, PM25_MODERATE
+    );
 
     //when
     int walkScore1 = walkScoreCalculator.calculateWalkScore(detail1);
@@ -101,10 +128,9 @@ class WalkScoreCalculatorTest {
   public void 대기질_매우나쁨() throws Exception {
     //given
     //pm2_5 76초과시 매우나쁨 -> 40점 감점
-    WeatherDetailInternal detail = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), 5, 77);
+    WeatherDetailInternal detail = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD, 77
+    );
 
     //when
     int walkScore = walkScoreCalculator.calculateWalkScore(detail);
@@ -117,15 +143,12 @@ class WalkScoreCalculatorTest {
   public void 대기질_나쁨() throws Exception {
     //given
     //pm2_5 36~75 나쁨 -> 20점 감점
-    WeatherDetailInternal detail1 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), 4, 75);
-
-    WeatherDetailInternal detail2 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), 3, 36);
+    WeatherDetailInternal detail1 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD, 75
+    );
+    WeatherDetailInternal detail2 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD, 36
+    );
 
     //when
     int walkScore1 = walkScoreCalculator.calculateWalkScore(detail1);
@@ -140,15 +163,12 @@ class WalkScoreCalculatorTest {
   public void 대기질_보통() throws Exception {
     //given
     //pm2_5 16~35 보통 -> 변동 없음
-    WeatherDetailInternal detail1 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), 3, 35);
-
-    WeatherDetailInternal detail2 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), 2, 16);
+    WeatherDetailInternal detail1 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD, 35
+    );
+    WeatherDetailInternal detail2 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD, 16
+    );
 
     //when
     int walkScore1 = walkScoreCalculator.calculateWalkScore(detail1);
@@ -163,15 +183,12 @@ class WalkScoreCalculatorTest {
   public void 대기질_좋음() throws Exception {
     //given
     //pm2_5 0~15 좋음 -> 10점 가점
-    WeatherDetailInternal detail1 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), 2, 15);
-
-    WeatherDetailInternal detail2 = new WeatherDetailInternal(
-        baseDetail.dateTime(), baseDetail.temperature(), baseDetail.humidity(),
-        baseDetail.weather(), baseDetail.precipitationProbability(), baseDetail.windSpeed(),
-        baseDetail.windDegree(), 1, 0);
+    WeatherDetailInternal detail1 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD, 15
+    );
+    WeatherDetailInternal detail2 = createWeatherDetail(
+        HIGH_TEMP_THRESHOLD, HIGH_HUMIDITY_THRESHOLD, 0
+    );
 
     //when
     int walkScore1 = walkScoreCalculator.calculateWalkScore(detail1);
