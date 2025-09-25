cd2c500247cc3bd74c82b416d5ef71529cedd56e
Jaehyeok Kim <bbabbasi0505@naver.com>
2025-09-25 13:24:57 +0900

    feat(walkscore): add low temperature penalty

--- src/main/java/com/kakao/termproject/weather/service/WalkScoreCalculator.java
+++ src/main/java/com/kakao/termproject/weather/service/WalkScoreCalculator.java
@@ -14,6 +14,9 @@ public class WalkScoreCalculator {
     if (temperature > 25) {
       score -= ((int) (temperature - 25)) * 5; // 25℃ 초과 시 1℃당 -5점
     }
+    if (temperature < 4) {
+      score -= ((int) (4 - temperature)) * 5; // 4℃ 미만일 경우 1℃당 -5점
+    }
 
     double humidity = detail.humidity();
     if (humidity > 60) {
@@ -23,13 +26,13 @@ public class WalkScoreCalculator {
     //대기질
     double pm2_5 = detail.pm2_5();
     if (pm2_5 >= 76) {
-      score -= 40;
+      score -= 40; //매우나쁨
     } else if (pm2_5 >= 36) {
-      score -= 20;
+      score -= 20; //나쁨
     } else if (pm2_5 >= 16) {
-      score += 0;
+      score += 0; //보통
     } else if (pm2_5 >= 0) {
-      score += 10;
+      score += 10; //좋음
     }
 
     return score;
