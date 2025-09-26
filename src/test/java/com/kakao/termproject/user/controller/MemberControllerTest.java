da7aab415bef2b85a432db3194ed3e6a182b6d21
happyGogildong <rbgus630@gmail.com>
2025-09-26 16:58:03 +0900

    test(Member): add test for member function - Login, Register, Set pet

---
+++ src/test/java/com/kakao/termproject/user/controller/MemberControllerTest.java
@@ -0,0 +1,68 @@
+package com.kakao.termproject.user.controller;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.kakao.termproject.pet.domain.Gender;
+import com.kakao.termproject.pet.dto.PetCreateRequest;
+import com.kakao.termproject.user.domain.Member;
+import com.kakao.termproject.user.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+class MemberControllerTest {
+
+  @InjectMocks
+  private MemberController memberController;
+
+  @Mock
+  private MemberService memberService;
+
+  @BeforeEach
+  void setUp() {
+
+  }
+
+  @Test
+  @DisplayName("반려견 등록 성공")
+  void 반려견_등록_성공(){
+
+    Member mockMember = mock(Member.class);
+    when(mockMember.getId()).thenReturn(1L);
+
+
+    PetCreateRequest petCreateRequest = new PetCreateRequest(
+        "testpet",
+        "testbreed",
+        Gender.MALE,
+        "2020-02-02",
+        false,
+        true,
+        10,
+        "sun",
+        "heart",
+        "asphalt"
+    );
+
+    ResponseEntity<Void> response = memberController.setPet(mockMember, petCreateRequest);
+
+    verify(memberService).setPet(1L, petCreateRequest);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+
+  }
+
+
+}
