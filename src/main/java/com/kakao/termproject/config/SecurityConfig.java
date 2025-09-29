1411de6360a27a6102168e9d27e8a643f6fb1657
ensalada.de.pollo <ensalada.de.pollo@90.local>
2025-09-29 19:18:49 +0900

    feat(cors): allow origin localhost:5173

--- src/main/java/com/kakao/termproject/config/SecurityConfig.java
+++ src/main/java/com/kakao/termproject/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("https://spring-gift.store"));
+    configuration.setAllowedOrigins(List.of("https://spring-gift.store", "http://localhost:5173"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of("*"));
     configuration.setAllowCredentials(true);
