e18c163296cf7e150e14ec42a46b5e2c424630e8
ensalada.de.pollo <ensalada.de.pollo@90.local>
2025-10-28 22:35:35 +0900

    feat: apply maximum character limit to title

--- src/main/java/com/kakao/termproject/post/dto/PostRequest.java
+++ src/main/java/com/kakao/termproject/post/dto/PostRequest.java
@@ -5,10 +5,11 @@ import jakarta.validation.constraints.Size;
 
 public record PostRequest(
   @NotBlank
+  @Size(min = 1, max = 20, message = "제목은 최대 20글자까지 입력 가능합니다.")
   String title,
 
   @NotBlank
-  @Size(min = 10, message = "10자 이상 입력해주세요.")
+  @Size(min = 10, max = 250, message = "내용은 10자 이상 250자 이하로 입력해주세요.")
   String content
 ) {
 
